7767c03a7e9586e1301806727c1f04a5f8fefd2a
BrendonGS <bgsggermany@gmail.com>
2019-04-25 09:14:25 -0500

    Basic Array

---
+++ basic_array.rb
@@ -0,0 +1,44 @@
+# Trello Board Ladder
+
+# Question 1
+
+# input: [1, 2, 3, 4]
+# output: 10
+
+# 1. Assess each number within the container one at a time
+# 2. Hold the value of the last numbedr assessed and add the next number within the containerf to it
+# 3. Make your way through the entirety of the container 
+# 4. Return the sum as result
+
+
+# def sum_of_numbers(numbers)
+#   sum = 0
+#   index = 0
+
+#   while index < numbers.length
+#     sum += numbers[index]
+#     index += 1
+#   end
+#     sum 
+# end
+
+# p sum_of_numbers([1, 2, 3, 4])
+
+# Question 2
+
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
+
+# array = [99, 101, 88, 4, 2000, 50]
+# new_array = []
+
+# array.each do |number|
+#     if number <= 100
+#       new_array << number
+#     end
+# end
+
+# p new_array
+
+# Question 3
